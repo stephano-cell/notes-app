e9832a53835c79da7fa739ae94b0770783a69296
stephano-cell <s.massouras@outlook.com>
2020-09-28 21:43:42 +0300

    Edit.html update note.title and note.body

--- notes-edit.js
+++ notes-edit.js
@@ -2,11 +2,32 @@ const noteId = location.hash.substring(1)
 const notes = getSavedNotes()
 const note=notes.find(function(note){
     return note.id===noteId
-})
+})  
 
 if (note===undefined){
     location.assign('/index.html')
 }
-
+//set values from getSaved notes for title and body
 document.querySelector('#note-title').value=note.title
 document.querySelector('#note-body').value=note.body
+
+
+//update note.title when user change value in iput
+document.querySelector('#note-title').addEventListener('input',function(e){
+   note.title=e.target.value
+    savedNotes(notes)
+})
+
+//update note.body when user changes value from form
+document.querySelector('#note-body').addEventListener('input',function(e){
+    note.body=e.target.value
+    savedNotes(notes)
+})
+
+//delete notes and redirect back to index page
+document.querySelector('#remove-note').addEventListener('click',function(){
+   removeNote(noteId)
+   savedNotes(notes)
+   location.assign('/index.html')
+   
+})
